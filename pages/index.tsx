50eeffc425396664b854649c82992912b873fd30
Chris Van Emmerik <chris@greenlinkservices.com>
2025-01-22 10:05:32 -0700

    Add iOS detection logic and installPrompt url parameter flag.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,40 @@
+import React from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  
+  const [isMounted, setIsMounted] = React.useState(false);
+  const [isIOS, setIsIOS] = React.useState(false);
+  const [isStandalone, setIsStandalone] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsIOS(
+      /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream
+    );
+
+    setIsStandalone(window.matchMedia("(display-mode: standalone)").matches);
+    setIsMounted(true);
+  }, []);
+
+  const installPrompt = isIOS && !isStandalone;
+  const src = `https://mindapps.org/pwa?installPrompt=${installPrompt}`;
+
+  console.log({ src, installPrompt, isIOS, isStandalone });
+
   return (
     <div>
       <main>
-        <iframe
-          style={{
-            display: "block",
-            height: "100vh",
-            width: "100vw",
-            border: "none",
-          }}
-          src="https://mindapps.org/pwa"
-        />
+        {isMounted && (
+          <iframe
+            style={{
+              display: "block",
+              height: "100vh",
+              width: "100vw",
+              border: "none",
+            }}
+            src={src}
+          />
+        )}
       </main>
     </div>
   );
