3e23f9b240a4793b8413c7d9d942aaab4717a996
Chris Van Emmerik <chris@greenlinkservices.com>
2025-01-22 11:54:42 -0700

    Update installed logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
 
+export const useUrlParameter = (paramName: any) => {
+  try {
+    var sPageURL = window.location.search.substring(1),
+      sURLVariables = sPageURL.split("&"),
+      sParameterName,
+      i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split("=");
+
+      if (sParameterName[0] === paramName) {
+        return sParameterName[1] === undefined
+          ? true
+          : decodeURIComponent(sParameterName[1]);
+      }
+    }
+    return undefined;
+  } catch (ex) {
+    return undefined;
+  }
+};
 export default function Home() {
-  
   const [isMounted, setIsMounted] = React.useState(false);
   const [isIOS, setIsIOS] = React.useState(false);
   const [isStandalone, setIsStandalone] = React.useState(false);
+
+  const installed = useUrlParameter("i");
 
   React.useEffect(() => {
     setIsIOS(
@@ -16,10 +38,10 @@ export default function Home() {
     setIsMounted(true);
   }, []);
 
-  const installPrompt = isIOS && !isStandalone;
+  const installPrompt = installed === "1" || (isIOS && !isStandalone);
   const src = `https://mindapps.org/pwa?installPrompt=${installPrompt}`;
 
-  console.log({ src, installPrompt, isIOS, isStandalone });
+  console.log({ src, i, installPrompt, isIOS, isStandalone });
 
   return (
     <div>
